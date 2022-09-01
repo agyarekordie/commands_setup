f8ac3f6c53928b2add59c66cefb1c4f4b18b0267
Christopher Agyare-Kordie <agyarekordie@yahoo.com>
2022-08-31 20:32:28 -0400

    additional information given on utilities

--- src/utilities/BrowserUtils.java
+++ src/utilities/BrowserUtils.java
@@ -3,6 +3,7 @@ package utilities;
 public class BrowserUtils {
     //html for utilities
     //utility information can be found here
+    //all utility information and bills can be accessed here
 
 
 }
